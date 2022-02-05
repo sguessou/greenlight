160153507cb61eaefe26149cbc64800b78983e84
Saad Guessous <guessous_saad@hotmail.com>
2022-02-05 19:02:23 +0200

    Send JSON response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,9 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	js := `{"status":"available", "environment":%q, "version":%q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }
