de9ecb8af9f329c3932cf3e24aa804cb304b7401
Saad Guessous <guessous_saad@hotmail.com>
2022-02-05 22:58:00 +0200

    Encode JSON map

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status":"available", "environment":%q, "version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(js))
