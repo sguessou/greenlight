be34163648ddeea442fc8254dfffbf8afba6e9e0
Saad Guessous <guessous_saad@hotmail.com>
2022-02-16 22:01:57 +0200

    Add logic for fetching a movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/sguessou/greenlight/internal/data"
 	"github.com/sguessou/greenlight/internal/validator"
@@ -63,13 +63,15 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance", "war"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
