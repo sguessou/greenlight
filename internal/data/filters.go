8131d0e619f8f0a704ad08639e08fff72788b471
Saad Guessous <guessous_saad@hotmail.com>
2022-02-27 14:03:32 +0200

    Add logic for sorting lists

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/sguessou/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/sguessou/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -16,4 +20,22 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
