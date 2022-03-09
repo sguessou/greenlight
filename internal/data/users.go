3c02a4c4169a9dd3f42e686ca8e93c866febda00
Saad Guessous <guessous_saad@hotmail.com>
2022-03-09 21:40:13 +0200

    Add validation for user struct

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/sguessou/greenlight/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -46,4 +47,30 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
