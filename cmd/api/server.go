e153e24ed6fc586622b82e315e9f6b6a23f446ae
Saad Guessous <guessous_saad@hotmail.com>
2022-03-16 21:30:40 +0200

    Implement graceful shutdown of background tasks

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -40,7 +40,17 @@ func (app *application) serve() error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		shutdownError <- srv.Shutdown(ctx)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+
+		app.logger.PrintInfo("completing background tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
