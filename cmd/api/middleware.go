78d6987f16c6a227a6207f4ec10a1358d8964fb1
Saad Guessous <guessous_saad@hotmail.com>
2022-03-26 10:03:23 +0200

    Add authorization middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -128,3 +128,21 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous() {
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+
+		if !user.Activated {
+			app.inactiveAccountResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
